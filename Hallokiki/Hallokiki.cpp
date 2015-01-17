996604ffca8ce22a2de1f49d4206dcc53867c830
Lulu <lulukmr@outlook.de>
2015-01-17 23:58:43 +0100

    Create Hallokiki.cpp

---
+++ Hallokiki/Hallokiki.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cmath>
+#include <string>
+using namespace std;
+
+int main()
+
+{
+		string  gruss = "Hallo Kikichen, mein Engelein!";
+		cout << gruss << endl;
+
+		cout << "Sinus von Pi ist:" << sin(3.141592) << endl;
+
+		return 0;
+}
