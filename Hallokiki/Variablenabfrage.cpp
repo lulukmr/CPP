12833bcbe64eeaf1c561f5a04cf17466c3ec2bf4
Lulu <lulukmr@outlook.de>
2015-01-18 21:07:45 +0100

    Update Variablenabfrage.cpp

--- Hallokiki/Variablenabfrage.cpp
+++ Hallokiki/Variablenabfrage.cpp
@@ -7,19 +7,19 @@ int main()
 {
 	int x = 0, z = 87;
 
-	cout << "Wert von x ist:" << x << endl;
-	cout << "wert von z ist:" << z << endl;
+	cout << "Wert von X ist:" << x << endl;
+	cout << "Wert von Z ist:" << z << endl;
 
 	x = 86;
 	x = x * z + 983;
 
-	cout << "wert von x ist:" << x << endl;
-	cout << "wert von z ist noch immer:" << z << endl;
+	cout << "Wert von X ist:" << x << endl;
+	cout << "Wert von Z ist noch immer:" << z << endl;
 
-	cout << "wert von x ist:" << x << endl;
+	cout << "Wert von X ist:" << x << endl;
 	
 	x = x + z;
-	cout << "jetzt ist x + z:" << x << endl;
+	cout << "Jetzt ist X + Z:" << x << endl;
 
 	return 0;
 }
