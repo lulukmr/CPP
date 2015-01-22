06e1f03e10d51278f1e8b02b540beee50704c13c
Lulu <lulukmr@outlook.de>
2015-01-22 19:23:36 +0100

    Update Hallokiki.cpp

--- Hallokiki/Hallokiki.cpp
+++ Hallokiki/Hallokiki.cpp
@@ -9,7 +9,8 @@ int main()
 		string  gruss = "Hallo Kikichen, mein Engelein!";
 		cout << gruss << endl;
 
-		cout << "Sinus von Pi ist:" << sin(3.141592) << endl;
+		cout << "Sinus von Pi ist:" << sin(3.141592) << endl << endl;
+		cout << "Bevor ich es vergesse, hab dich ganz doll lieb!" << endl;
 
 		return 0;
 }
